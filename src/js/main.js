13a4fb883b0ade91a023d519f0eb726c72784d4f
Thomas Wilburn <news@thomaswilburn.net>
2015-06-08 12:46:13 -0700

    Columns using ng-repeat

--- src/js/main.js
+++ src/js/main.js
@@ -8,14 +8,29 @@ var angular = require("angular");
 
 var app = angular.module("discipline-app", []);
 
+app.filter("na", function() {
+  return function(value) {
+    return value === false ? "N/A" : value;
+  }
+})
+
 app.controller("discipline-controller", ["$scope", "$filter", function($scope, $filter) {
 
   var ngNumber = $filter("number");
 
   var all = window.discipline.filter(function(row) {
-    return true;
+    if (true) return true;
     //line beneath filters for null rows, suppress right now for transparency
     return "asian_d,black_d,hispanic_d,multi_d,li_d".split(",").some((k) => row[k]);
+  });
+
+  all.forEach(function(item) {
+    "asian,black,hispanic,multi,li,white".split(",").forEach(function(demo) {
+      item[`${demo}_d`] = (item[`${demo}_d`] * 1) || false;
+      item[`${demo}_pop`] = (item[`${demo}_pop`] * 1) || 0;
+      var rate = item[`${demo}_d`] / item[`${demo}_pop`];
+      item[`${demo}_rate`] = (rate * 100) || false;
+    });
   });
 
   $scope.search = "";
